9a12abb32ba7f5d7554dcc6eac41b3e3f9376fb7
Holman Daniel Quintero Salazar <hd.quinterosalazar@ugto.mx>
2025-09-22 15:55:51 -0600

    Arquitectura

--- app.js
+++ app.js
@@ -1,1 +1,75 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+/* Autor: Holman Quintero Salazar
+ ALURA_latam
+ Repositorio del Challenge Amigo secreto. ALURA Latam. Principiante en Programación G9 - ONE.
+ */
+
+
+
+
+let amigo_n = ''; // Declaracion de la variable amigo cada vez que se hace click en el boton añadir
+
+const lista_amigos = [];
+
+
+//function start(){
+
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function agregarAmigo() {
+    //amigo_n = document.getElementById('amigo').value;
+    //console.log(amigo_n)
+    //let elementoHTML = document.getElementById('listaAmigos');
+    //elementoHTML.innerHTML = amigo_n
+
+    //lista_amigos++;
+
+    //limpiar_caja();
+    //return amigo_n;
+}
+
+console.log(amigo_n)
+
+
+function limpiar_caja(){
+    let inputElement = document.getElementById('amigo');
+    inputElement.setAttribute('placeholder', 'Escribe un nombre o sortea');
+}
+
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let length = fruits.length;
+
+
+
+
+
+function sortearAmigo() {
+    alert("F2")
+}
+
+
+
+
+
+
+
+
